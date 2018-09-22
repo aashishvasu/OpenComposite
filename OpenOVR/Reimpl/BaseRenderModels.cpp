840542eaf9ec93b4b4501af597bf96c2712e80ea
Campbell Suter <znix@znix.xyz>
2018-09-22 15:48:26 +1200

    Move the RenderModels code out of it's interface class and into a base class

---
+++ OpenOVR/Reimpl/BaseRenderModels.cpp
@@ -0,0 +1,138 @@
+#include "stdafx.h"
+#define BASE_IMPL
+#include "BaseRenderModels.h"
+
+#pragma region structs
+
+enum OOVR_EVRRenderModelError {
+	VRRenderModelError_None = 0,
+	VRRenderModelError_Loading = 100,
+	VRRenderModelError_NotSupported = 200,
+	VRRenderModelError_InvalidArg = 300,
+	VRRenderModelError_InvalidModel = 301,
+	VRRenderModelError_NoShapes = 302,
+	VRRenderModelError_MultipleShapes = 303,
+	VRRenderModelError_TooManyVertices = 304,
+	VRRenderModelError_MultipleTextures = 305,
+	VRRenderModelError_BufferTooSmall = 306,
+	VRRenderModelError_NotEnoughNormals = 307,
+	VRRenderModelError_NotEnoughTexCoords = 308,
+
+	VRRenderModelError_InvalidTexture = 400,
+};
+
+struct OOVR_RenderModel_Vertex_t {
+	vr::HmdVector3_t vPosition;		// position in meters in device space
+	vr::HmdVector3_t vNormal;
+	float rfTextureCoord[2];
+};
+
+#if defined(__linux__) || defined(__APPLE__) 
+// This structure was originally defined mis-packed on Linux, preserved for 
+// compatibility. 
+#pragma pack( push, 4 )
+#endif
+
+struct OOVR_RenderModel_t {
+	const OOVR_RenderModel_Vertex_t *rVertexData;	// Vertex data for the mesh
+	uint32_t unVertexCount;						// Number of vertices in the vertex data
+	const uint16_t *rIndexData;					// Indices into the vertex data for each triangle
+	uint32_t unTriangleCount;					// Number of triangles in the mesh. Index count is 3 * TriangleCount
+	OOVR_TextureID_t diffuseTextureId;			// Session unique texture identifier. Rendermodels which share the same texture will have the same id. <0 == texture not present
+};
+
+struct OOVR_RenderModel_TextureMap_t {
+	uint16_t unWidth, unHeight; // width and height of the texture map in pixels
+	const uint8_t *rubTextureMapData;	// Map texture data. All textures are RGBA with 8 bits per channel per pixel. Data size is width * height * 4ub
+};
+
+#if defined(__linux__) || defined(__APPLE__) 
+#pragma pack( pop )
+#endif
+
+#pragma endregion
+
+typedef OOVR_RenderModel_t RenderModel_t;
+typedef OOVR_EVRRenderModelError EVRRenderModelError;
+typedef OOVR_RenderModel_TextureMap_t RenderModel_TextureMap_t;
+typedef OOVR_TextureID_t TextureID_t;
+
+EVRRenderModelError BaseRenderModels::LoadRenderModel_Async(const char * pchRenderModelName, RenderModel_t ** renderModel) {
+	*renderModel = new RenderModel_t();
+	// TODO use Oculus Avatars SDK
+
+	return VRRenderModelError_None;
+}
+
+void BaseRenderModels::FreeRenderModel(RenderModel_t * renderModel) {
+	// TODO
+	delete renderModel;
+}
+
+EVRRenderModelError BaseRenderModels::LoadTexture_Async(TextureID_t textureId, RenderModel_TextureMap_t ** texture) {
+	*texture = new RenderModel_TextureMap_t();
+	// TODO use Oculus Avatars SDK
+
+	return VRRenderModelError_None;
+}
+
+void BaseRenderModels::FreeTexture(RenderModel_TextureMap_t * texture) {
+	// TODO
+	delete texture;
+}
+
+EVRRenderModelError BaseRenderModels::LoadTextureD3D11_Async(TextureID_t textureId, void * pD3D11Device, void ** ppD3D11Texture2D) {
+	throw "stub";
+}
+
+EVRRenderModelError BaseRenderModels::LoadIntoTextureD3D11_Async(TextureID_t textureId, void * pDstTexture) {
+	throw "stub";
+}
+
+void BaseRenderModels::FreeTextureD3D11(void * pD3D11Texture2D) {
+	throw "stub";
+}
+
+uint32_t BaseRenderModels::GetRenderModelName(uint32_t unRenderModelIndex, VR_OUT_STRING() char * pchRenderModelName, uint32_t unRenderModelNameLen) {
+	throw "stub";
+}
+
+uint32_t BaseRenderModels::GetRenderModelCount() {
+	throw "stub";
+}
+
+uint32_t BaseRenderModels::GetComponentCount(const char * pchRenderModelName) {
+	throw "stub";
+}
+
+uint32_t BaseRenderModels::GetComponentName(const char * pchRenderModelName, uint32_t unComponentIndex, VR_OUT_STRING() char * pchComponentName, uint32_t unComponentNameLen) {
+	throw "stub";
+}
+
+uint64_t BaseRenderModels::GetComponentButtonMask(const char * pchRenderModelName, const char * pchComponentName) {
+	throw "stub";
+}
+
+uint32_t BaseRenderModels::GetComponentRenderModelName(const char * pchRenderModelName, const char * pchComponentName, VR_OUT_STRING() char * pchComponentRenderModelName, uint32_t unComponentRenderModelNameLen) {
+	throw "stub";
+}
+
+bool BaseRenderModels::GetComponentState(const char * pchRenderModelName, const char * pchComponentName, const vr::VRControllerState_t * pControllerState, const OOVR_RenderModel_ControllerMode_State_t * pState, OOVR_RenderModel_ComponentState_t * pComponentState) {
+	throw "stub";
+}
+
+bool BaseRenderModels::RenderModelHasComponent(const char * pchRenderModelName, const char * pchComponentName) {
+	throw "stub";
+}
+
+uint32_t BaseRenderModels::GetRenderModelThumbnailURL(const char * pchRenderModelName, VR_OUT_STRING() char * pchThumbnailURL, uint32_t unThumbnailURLLen, EVRRenderModelError * peError) {
+	throw "stub";
+}
+
+uint32_t BaseRenderModels::GetRenderModelOriginalPath(const char * pchRenderModelName, VR_OUT_STRING() char * pchOriginalPath, uint32_t unOriginalPathLen, EVRRenderModelError * peError) {
+	throw "stub";
+}
+
+const char * BaseRenderModels::GetRenderModelErrorNameFromEnum(EVRRenderModelError error) {
+	throw "stub";
+}
--- OpenOVR/Reimpl/CVRRenderModels005.cpp
+++ OpenOVR/Reimpl/CVRRenderModels005.cpp
@@ -1,84 +1,62 @@
 #include "stdafx.h"
 #define CVR_IMPL
 #include "CVRRenderModels005.h"
+
 CVR_GEN_IMPL(CVRRenderModels_005);
 
-EVRRenderModelError CVRRenderModels_005::LoadRenderModel_Async(const char * pchRenderModelName, RenderModel_t ** renderModel) {
-	*renderModel = new RenderModel_t();
-	// TODO use Oculus Avatars SDK
-
-	return VRRenderModelError_None;
+EVRRenderModelError CVRRenderModels_005::LoadRenderModel_Async(const char * pchRenderModelName, RenderModel_t ** ppRenderModel) {
+	return (EVRRenderModelError) base.LoadRenderModel_Async(pchRenderModelName, (OOVR_RenderModel_t**) ppRenderModel);
 }
-
-void CVRRenderModels_005::FreeRenderModel(RenderModel_t * renderModel) {
-	// TODO
-	delete renderModel;
+void CVRRenderModels_005::FreeRenderModel(RenderModel_t * pRenderModel) {
+	return base.FreeRenderModel((OOVR_RenderModel_t*) pRenderModel);
 }
-
-EVRRenderModelError CVRRenderModels_005::LoadTexture_Async(TextureID_t textureId, RenderModel_TextureMap_t ** texture) {
-	*texture = new RenderModel_TextureMap_t();
-	// TODO use Oculus Avatars SDK
-
-	return VRRenderModelError_None;
+EVRRenderModelError CVRRenderModels_005::LoadTexture_Async(TextureID_t textureId, RenderModel_TextureMap_t ** ppTexture) {
+	return (EVRRenderModelError)base.LoadTexture_Async(textureId, (OOVR_RenderModel_TextureMap_t**) ppTexture);
 }
-
-void CVRRenderModels_005::FreeTexture(RenderModel_TextureMap_t * texture) {
-	// TODO
-	delete texture;
+void CVRRenderModels_005::FreeTexture(RenderModel_TextureMap_t * pTexture) {
+	return base.FreeTexture((OOVR_RenderModel_TextureMap_t*) pTexture);
 }
-
 EVRRenderModelError CVRRenderModels_005::LoadTextureD3D11_Async(TextureID_t textureId, void * pD3D11Device, void ** ppD3D11Texture2D) {
-	throw "stub";
+	return (EVRRenderModelError) base.LoadTextureD3D11_Async(textureId, pD3D11Device, ppD3D11Texture2D);
 }
-
 EVRRenderModelError CVRRenderModels_005::LoadIntoTextureD3D11_Async(TextureID_t textureId, void * pDstTexture) {
-	throw "stub";
+	return (EVRRenderModelError) base.LoadIntoTextureD3D11_Async(textureId, pDstTexture);
 }
-
 void CVRRenderModels_005::FreeTextureD3D11(void * pD3D11Texture2D) {
-	throw "stub";
+	return base.FreeTextureD3D11(pD3D11Texture2D);
 }
-
 uint32_t CVRRenderModels_005::GetRenderModelName(uint32_t unRenderModelIndex, VR_OUT_STRING() char * pchRenderModelName, uint32_t unRenderModelNameLen) {
-	throw "stub";
+	return base.GetRenderModelName(unRenderModelIndex, pchRenderModelName, unRenderModelNameLen);
 }
-
 uint32_t CVRRenderModels_005::GetRenderModelCount() {
-	throw "stub";
+	return base.GetRenderModelCount();
 }
-
 uint32_t CVRRenderModels_005::GetComponentCount(const char * pchRenderModelName) {
-	throw "stub";
+	return base.GetComponentCount(pchRenderModelName);
 }
-
 uint32_t CVRRenderModels_005::GetComponentName(const char * pchRenderModelName, uint32_t unComponentIndex, VR_OUT_STRING() char * pchComponentName, uint32_t unComponentNameLen) {
-	throw "stub";
+	return base.GetComponentName(pchRenderModelName, unComponentIndex, pchComponentName, unComponentNameLen);
 }
-
 uint64_t CVRRenderModels_005::GetComponentButtonMask(const char * pchRenderModelName, const char * pchComponentName) {
-	throw "stub";
+	return base.GetComponentButtonMask(pchRenderModelName, pchComponentName);
 }
-
 uint32_t CVRRenderModels_005::GetComponentRenderModelName(const char * pchRenderModelName, const char * pchComponentName, VR_OUT_STRING() char * pchComponentRenderModelName, uint32_t unComponentRenderModelNameLen) {
-	throw "stub";
+	return base.GetComponentRenderModelName(pchRenderModelName, pchComponentName, pchComponentRenderModelName, unComponentRenderModelNameLen);
 }
-
-bool CVRRenderModels_005::GetComponentState(const char * pchRenderModelName, const char * pchComponentName, const vr::VRControllerState_t * pControllerState, const RenderModel_ControllerMode_State_t * pState, RenderModel_ComponentState_t * pComponentState) {
-	throw "stub";
+bool CVRRenderModels_005::GetComponentState(const char * pchRenderModelName, const char * pchComponentName, const VRControllerState_t * pControllerState, const RenderModel_ControllerMode_State_t * pState, RenderModel_ComponentState_t * pComponentState) {
+	return base.GetComponentState(pchRenderModelName, pchComponentName, pControllerState,
+		(const OOVR_RenderModel_ControllerMode_State_t*) pState,
+		(OOVR_RenderModel_ComponentState_t*) pComponentState);
 }
-
 bool CVRRenderModels_005::RenderModelHasComponent(const char * pchRenderModelName, const char * pchComponentName) {
-	throw "stub";
+	return base.RenderModelHasComponent(pchRenderModelName, pchComponentName);
 }
-
 uint32_t CVRRenderModels_005::GetRenderModelThumbnailURL(const char * pchRenderModelName, VR_OUT_STRING() char * pchThumbnailURL, uint32_t unThumbnailURLLen, EVRRenderModelError * peError) {
-	throw "stub";
+	return base.GetRenderModelThumbnailURL(pchRenderModelName, pchThumbnailURL, unThumbnailURLLen, (OOVR_EVRRenderModelError*) peError);
 }
-
 uint32_t CVRRenderModels_005::GetRenderModelOriginalPath(const char * pchRenderModelName, VR_OUT_STRING() char * pchOriginalPath, uint32_t unOriginalPathLen, EVRRenderModelError * peError) {
-	throw "stub";
+	return base.GetRenderModelOriginalPath(pchRenderModelName, pchOriginalPath, unOriginalPathLen, (OOVR_EVRRenderModelError*) peError);
 }
-
 const char * CVRRenderModels_005::GetRenderModelErrorNameFromEnum(EVRRenderModelError error) {
-	throw "stub";
+	return base.GetRenderModelErrorNameFromEnum((OOVR_EVRRenderModelError) error);
 }
