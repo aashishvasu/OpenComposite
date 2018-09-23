8cfb8959aa4bc456220c6e3596c6a7ec1764ba1b
Campbell Suter <znix@znix.xyz>
2018-09-24 01:18:48 +1200

    Add IVRCompositor version 021 stub

--- OpenOVR/Reimpl/CVRCompositor.cpp
+++ OpenOVR/Reimpl/CVRCompositor.cpp
@@ -4,6 +4,7 @@
 
 GEN_INTERFACE("Compositor", "019")
 GEN_INTERFACE("Compositor", "020")
+GEN_INTERFACE("Compositor", "021")
 GEN_INTERFACE("Compositor", "022")
 
 #include "GVRCompositor.gen.h"
@@ -12,6 +13,10 @@ GEN_INTERFACE("Compositor", "022")
 // This didn't happen before due to not knowing how to
 // handle structs, but not there isn't any reason why not.
 void CVRCompositor_022::GetCumulativeStats(vr::IVRCompositor_022::Compositor_CumulativeStats* pStats, uint32_t nStatsSizeInBytes) {
+	STUBBED();
+}
+
+void CVRCompositor_021::GetCumulativeStats(vr::IVRCompositor_021::Compositor_CumulativeStats* pStats, uint32_t nStatsSizeInBytes) {
 	STUBBED();
 }
 
