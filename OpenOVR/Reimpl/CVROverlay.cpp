6ad307aa6c180792a393f7e1b2b784a560edf4e2
Campbell Suter <znix@znix.xyz>
2018-09-23 16:51:28 +1200

    Add IVROverlay version 014

--- OpenOVR/Reimpl/CVROverlay.cpp
+++ OpenOVR/Reimpl/CVROverlay.cpp
@@ -2,6 +2,8 @@
 #define GENFILE
 #include "BaseCommon.h"
 
+GEN_INTERFACE("Overlay", "014")
+// TODO 015
 GEN_INTERFACE("Overlay", "016")
 GEN_INTERFACE("Overlay", "017")
 GEN_INTERFACE("Overlay", "018")
