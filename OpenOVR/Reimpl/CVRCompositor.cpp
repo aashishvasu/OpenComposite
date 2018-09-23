c6a0bd3ebc6f49ec6a86687b70a14f0a779c8d59
Campbell Suter <znix@znix.xyz>
2018-09-23 16:51:10 +1200

    Add IVRCompositor version 019

--- OpenOVR/Reimpl/CVRCompositor.cpp
+++ OpenOVR/Reimpl/CVRCompositor.cpp
@@ -2,6 +2,7 @@
 #define GENFILE
 #include "BaseCommon.h"
 
+GEN_INTERFACE("Compositor", "019")
 GEN_INTERFACE("Compositor", "020")
 GEN_INTERFACE("Compositor", "022")
 
@@ -15,5 +16,9 @@ void CVRCompositor_022::GetCumulativeStats(vr::IVRCompositor_022::Compositor_Cum
 }
 
 void CVRCompositor_020::GetCumulativeStats(vr::IVRCompositor_020::Compositor_CumulativeStats* pStats, uint32_t nStatsSizeInBytes) {
+	STUBBED();
+}
+
+void CVRCompositor_019::GetCumulativeStats(vr::IVRCompositor_019::Compositor_CumulativeStats* pStats, uint32_t nStatsSizeInBytes) {
 	STUBBED();
 }
