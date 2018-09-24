d62b71dc4711751ff5412588dc5b318071b7c5e8
Campbell Suter <znix@znix.xyz>
2018-09-24 15:24:33 +1200

    Add stub for IVRSystem version 012 - Fixes #7

--- OpenOVR/Reimpl/CVRSystem.cpp
+++ OpenOVR/Reimpl/CVRSystem.cpp
@@ -3,6 +3,7 @@
 #include "BaseCommon.h"
 
 GEN_INTERFACE("System", "011")
+GEN_INTERFACE("System", "012")
 //
 GEN_INTERFACE("System", "015")
 GEN_INTERFACE("System", "016")
@@ -19,5 +20,14 @@ bool CVRSystem_011::GetControllerState(vr::TrackedDeviceIndex_t unControllerDevi
 
 bool CVRSystem_011::GetControllerStateWithPose(vr::ETrackingUniverseOrigin eOrigin, vr::TrackedDeviceIndex_t unControllerDeviceIndex,
 		vr::VRControllerState_t* pControllerState, vr::TrackedDevicePose_t* pTrackedDevicePose) {
+	return base.GetControllerStateWithPose(eOrigin, unControllerDeviceIndex, pControllerState, sizeof(vr::VRControllerState001_t), pTrackedDevicePose);
+}
+
+bool CVRSystem_012::GetControllerState(vr::TrackedDeviceIndex_t unControllerDeviceIndex, vr::VRControllerState_t* pControllerState) {
+	return base.GetControllerState(unControllerDeviceIndex, pControllerState, sizeof(vr::VRControllerState001_t));
+}
+
+bool CVRSystem_012::GetControllerStateWithPose(vr::ETrackingUniverseOrigin eOrigin, vr::TrackedDeviceIndex_t unControllerDeviceIndex,
+	vr::VRControllerState_t* pControllerState, vr::TrackedDevicePose_t* pTrackedDevicePose) {
 	return base.GetControllerStateWithPose(eOrigin, unControllerDeviceIndex, pControllerState, sizeof(vr::VRControllerState001_t), pTrackedDevicePose);
 }
