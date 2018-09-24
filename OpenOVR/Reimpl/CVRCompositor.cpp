d521866d195f1c4000281ced23948d43b10b2fd2
Campbell Suter <znix@znix.xyz>
2018-09-24 15:41:00 +1200

    Add stub for IVRCompositor version 016

--- OpenOVR/Reimpl/CVRCompositor.cpp
+++ OpenOVR/Reimpl/CVRCompositor.cpp
@@ -3,6 +3,7 @@
 #include "BaseCommon.h"
 
 GEN_INTERFACE("Compositor", "013")
+GEN_INTERFACE("Compositor", "016")
 GEN_INTERFACE("Compositor", "019")
 GEN_INTERFACE("Compositor", "020")
 GEN_INTERFACE("Compositor", "021")
