382c93a1525c0d8c3f0bc7707759dc749d4fcda6
Campbell Suter <znix@znix.xyz>
2018-09-26 21:52:44 +1200

    Add stub for IVROverlay version 013 (See #21)

--- OpenOVR/Reimpl/CVROverlay.cpp
+++ OpenOVR/Reimpl/CVROverlay.cpp
@@ -4,6 +4,8 @@
 
 GEN_INTERFACE("Overlay", "010")
 GEN_INTERFACE("Overlay", "011")
+// TODO 012
+GEN_INTERFACE("Overlay", "013")
 GEN_INTERFACE("Overlay", "014")
 // TODO 015
 GEN_INTERFACE("Overlay", "016")
@@ -13,6 +15,14 @@ GEN_INTERFACE("Overlay", "018")
 #include "GVROverlay.gen.h"
 
 vr::EVROverlayError CVROverlay_011::GetOverlayTexture(vr::VROverlayHandle_t ulOverlayHandle, void** pNativeTextureHandle,
+	void* pNativeTextureRef, uint32_t* pWidth, uint32_t* pHeight, uint32_t* pNativeFormat, EGraphicsAPIConvention* pAPI,
+	vr::EColorSpace* pColorSpace) {
+
+	// It should be fairly simple, but it's unlikely to be used so I can't be bothered implementing it now
+	STUBBED();
+}
+
+vr::EVROverlayError CVROverlay_013::GetOverlayTexture(vr::VROverlayHandle_t ulOverlayHandle, void** pNativeTextureHandle,
 	void* pNativeTextureRef, uint32_t* pWidth, uint32_t* pHeight, uint32_t* pNativeFormat, EGraphicsAPIConvention* pAPI,
 	vr::EColorSpace* pColorSpace) {
 
