56004e1ae23e3484970491e15f84f0952f769886
Campbell Suter <znix@znix.xyz>
2018-09-26 21:06:38 +1200

    Add initial haptics support

--- OpenOVR/Misc/Haptics.cpp
+++ OpenOVR/Misc/Haptics.cpp
@@ -20,19 +20,33 @@ void Haptics::StartSimplePulse(ovrControllerType ctrl, unsigned int durationMicr
 		left = false;
 	}
 	else {
-		// TODO
-		return;
+		OOVR_ABORT("invalid controller");
 	}
 
-	/*
-	// TODO amplitude and frequency settings
-	ovr_SetControllerVibration(*ovr::session, ctrl, 0.5, 0.5);
+	int numSamples = durationMicroseconds * 320.0f / 1000000.0f;
 
-	// Use async to launch a function (lambda) in parallel
-	std::async(std::launch::async, [ctrl, durationMicroseconds]() {
-		std::this_thread::sleep_for(std::chrono::microseconds(durationMicroseconds));
+	if (numSamples > OVR_HAPTICS_BUFFER_SAMPLES_MAX)
+		numSamples = OVR_HAPTICS_BUFFER_SAMPLES_MAX;
+	else if (numSamples < 1)
+		numSamples = 1;
 
-		ovr_SetControllerVibration(*ovr::session, ctrl, 0.5, 0.5);
-	});
-	*/
+	uint8_t *samples = new uint8_t[numSamples];
+
+	// Generate the samples
+	for (int i = 0; i < numSamples; i++) {
+		// TODO should we use 160Hz?
+		samples[i] = 255;
+	}
+
+	ovrHapticsBuffer buff;
+	buff.SubmitMode = ovrHapticsBufferSubmit_Enqueue;
+	buff.SamplesCount = numSamples;
+	buff.Samples = samples;
+
+	// TODO queue it up if the controller is busy
+	// This should make the haptics work *much* better.
+	ovr_SubmitControllerVibration(*ovr::session, ctrl, &buff);
+
+	// Free up memory
+	delete samples;
 }
