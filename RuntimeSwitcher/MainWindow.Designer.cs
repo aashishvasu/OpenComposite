0a70fca5b69a17c5e0d631cc34174929620f6c37
Campbell Suter <znix@znix.xyz>
2018-10-13 10:08:09 +1300

    RuntimeSwitcher: Initial UI

---
+++ RuntimeSwitcher/MainWindow.Designer.cs
@@ -0,0 +1,109 @@
+namespace RuntimeSwitcher
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.useOpenComposite = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Status:";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(60, 13);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(45, 13);
+            this.statusLabel.TabIndex = 1;
+            this.statusLabel.Text = "Loading";
+            // 
+            // useOpenComposite
+            // 
+            this.useOpenComposite.Location = new System.Drawing.Point(16, 30);
+            this.useOpenComposite.Name = "useOpenComposite";
+            this.useOpenComposite.Size = new System.Drawing.Size(334, 23);
+            this.useOpenComposite.TabIndex = 2;
+            this.useOpenComposite.Text = "Switch to OpenComposite";
+            this.useOpenComposite.UseVisualStyleBackColor = true;
+            this.useOpenComposite.Click += new System.EventHandler(this.useOpenComposite_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(334, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "You can switch back to SteamVR by starting it via Steam\'s VR button";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(19, 77);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(331, 23);
+            this.progressBar.TabIndex = 4;
+            this.progressBar.Visible = false;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 142);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.useOpenComposite);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "MainWindow";
+            this.Text = "OpenComposite Runtime Switcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Button useOpenComposite;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ RuntimeSwitcher/MainWindow.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RuntimeSwitcher
+{
+    public partial class MainWindow : Form
+    {
+        private ConfigReader config;
+        private string ocRuntimePath;
+
+        public MainWindow()
+        {
+            ocRuntimePath = Directory.GetParent(System.Reflection.Assembly.GetEntryAssembly().Location).FullName;
+            ocRuntimePath += Path.DirectorySeparatorChar + "OpenComposite";
+
+            if (!Directory.Exists(ocRuntimePath)) {
+                Directory.CreateDirectory(ocRuntimePath);
+            }
+
+            config = new ConfigReader();
+
+            InitializeComponent();
+
+            UpdateStatus();
+        }
+
+        private void UpdateStatus()
+        {
+            string runtime = config.Runtimes.Count == 0 ? null : config.Runtimes.First();
+            useOpenComposite.Enabled = true;
+
+            if (runtime == null)
+            {
+                statusLabel.Text = "None";
+            }
+            else if (runtime.Contains("SteamVR"))
+            {
+                statusLabel.Text = "SteamVR";
+            }
+            else if (runtime == ocRuntimePath)
+            {
+                statusLabel.Text = "OpenComposite";
+                useOpenComposite.Enabled = false;
+            }
+            else
+            {
+                statusLabel.Text = "Other";
+            }
+        }
+
+        private void SwitchToOpenComposite()
+        {
+            List<string> rts = new List<string>(config.Runtimes);
+            rts.RemoveAll(s => s == ocRuntimePath);
+            rts.Insert(0, ocRuntimePath);
+            config.Runtimes = rts;
+
+            UpdateStatus();
+        }
+
+        private async void useOpenComposite_Click(object sender, EventArgs e)
+        {
+            useOpenComposite.Enabled = false;
+
+            bool downloads = false;
+
+            string vrclient = ocRuntimePath + Path.DirectorySeparatorChar + "vrclient.dll";
+            if (!File.Exists(vrclient))
+            {
+                downloads = true;
+                statusLabel.Text = "Downloading 32-bit DLL";
+                await DownloadFile(vrclient, "https://znix.xyz/OpenComposite/download.php?arch=x86");
+            }
+
+            string vrclient_x64 = ocRuntimePath + Path.DirectorySeparatorChar + "vrclient_x64.dll";
+            if (!File.Exists(vrclient_x64))
+            {
+                downloads = true;
+                statusLabel.Text = "Downloading 64-bit DLL";
+                await DownloadFile(vrclient_x64, "https://znix.xyz/OpenComposite/download.php?arch=x64");
+            }
+
+            SwitchToOpenComposite();
+
+            if(downloads)
+            {
+                statusLabel.Text += " (Download Complete)";
+            }
+        }
+
+        private async Task DownloadFile(string name, string url)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                progressBar.Visible = true;
+
+                wc.DownloadProgressChanged += (object sender, DownloadProgressChangedEventArgs e) =>
+                {
+                    progressBar.Value = e.ProgressPercentage;
+                };
+
+                // Ensure the file gets redownloaded if the program is closed during download
+                string temp = name + ".part";
+
+                await wc.DownloadFileTaskAsync(
+                    // Param1 = Link of file
+                    new System.Uri(url),
+                    // Param2 = Path to save
+                    temp
+                );
+
+                File.Move(temp, name);
+
+                progressBar.Value = 0;
+                progressBar.Visible = false;
+            }
+        }
+    }
+}
