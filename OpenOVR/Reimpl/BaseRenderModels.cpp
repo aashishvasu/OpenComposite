f39babc1d1d122572e355b455f4c840b61eb955b
Campbell Suter <znix@znix.xyz>
2018-12-14 14:10:45 +1300

    Fix memory leak in LoadIntoTextureD3D11_Async

--- OpenOVR/Reimpl/BaseRenderModels.cpp
+++ OpenOVR/Reimpl/BaseRenderModels.cpp
@@ -324,6 +324,7 @@ EVRRenderModelError BaseRenderModels::LoadIntoTextureD3D11_Async(TextureID_t tex
 	// Cleanup
 	tempTex->Release();
 	delete init;
+	delete[] pixels;
 	context->Release();
 	device->Release();
 
