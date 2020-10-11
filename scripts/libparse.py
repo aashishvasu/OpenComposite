19f70cb919a90e1353339c269cc299b4c2b44c50
Campbell Suter <znix@znix.xyz>
2020-10-11 15:35:19 +1300

    Rewrite the stub generation script

--- scripts/libparse.py
+++ scripts/libparse.py
@@ -15,7 +15,18 @@ typedef = re.compile(r"^typedef\s+(?P<def>[\w\s\d_\*&]+)\s+(?P<name>\w+);$")
 typedef_struct = re.compile(r"^typedef\s+(?:struct|union)$")
 typedef_struct_end = re.compile(r"^}\s+(?P<name>\w+);$")
 
+
 def nice_lines(fi):
+    """
+    Process lines of C code to handle multi-line stuff. For example if a line ends
+    with a backstroke or a comma, then the subsequent line is merged into it.
+
+    Eventually this function may be imporoved to handle (and possibly remove) comments.
+
+    :param fi: An iterator object to read the input lines from
+    :return: A list of strings, one per line
+    """
+
     lines = []
     carry = None
 
@@ -37,7 +48,32 @@ def nice_lines(fi):
 
     return lines
 
+
 def read_context(context, filename, namespace):
+    """
+    Read a header file looking for struct, enum and typedef declarations. These names are
+    then saved into the context, and prepended with the given namespace.
+
+    For example, if a sample file was:
+
+    struct abc
+    {}
+    enum def
+    {}
+    typedef int hij;
+
+    Then the following values would be inserted into context, if the namespace param was 'my_ns':
+
+    abc = my_ns::abc
+    def = my_ns::def
+    hij = my_ns::hij
+
+    :param context: A dict mapping the names of structs/enums/typedefs to their namespace-qualified name
+    :param filename: The path to the file - either a string or pathlib Path
+    :param namespace: The namespace to be prepended to the typenames. If this is an empty string, types will
+    be prepended with two colons.
+    """
+
     if not namespace:
         raise RuntimeError("Automatic namespace detection not yet implemented! Please specify a namespace!")
 
@@ -58,7 +94,7 @@ def read_context(context, filename, namespace):
 
             if match:
                 name = match.group("name")
-                context[name] = namespace + "::" + name # TODO
+                context[name] = namespace + "::" + name  # TODO
             else:
                 match = typedef_struct.match(line)
                 if match:
@@ -67,15 +103,16 @@ def read_context(context, filename, namespace):
         if tdstruct_start:
             raise Exception("Cound not find closing for structure '%s'" % tdstruct_start)
 
+
 def convert_type(t, context):
     # First, remove any spaces and pointer/reference symbols, setting them aside
     ptr_str = ""
     while True:
         if t[-1].isspace():
-            t=t[0:-1]
+            t = t[0:-1]
         elif t[-1] == "*" or t[-1] == "&":
             ptr_str += t[-1]
-            t=t[0:-1]
+            t = t[0:-1]
         else:
             break
 
@@ -87,7 +124,7 @@ def convert_type(t, context):
     # Note these macros are unimportant, spplying attributes to Clang.
     # If necessary these can be saved and added back on later.
     if "(" in t:
-        t = t[t.index(")")+1:]
+        t = t[t.index(")") + 1:]
         t = t.lstrip()
 
     # Strip off the const, if applicable
@@ -106,6 +143,7 @@ def convert_type(t, context):
 
     # Add the pointer/reference symbols back in
     return t + ptr_str
+
 
 def parseline(line, context=None):
     line = line.strip()
@@ -132,7 +170,7 @@ def parseline(line, context=None):
             if adefault:
                 adefault = adefault.strip()
 
-            args.append(arg_t( type=atype, name=aname, default=adefault, str=fullarg ))
+            args.append(arg_t(type=atype, name=aname, default=adefault, str=fullarg))
 
     funcdef = funcdef_t(return_type=ret, name=name, args=args)
     return funcdef
---
+++ scripts/stubs/interface.py
@@ -0,0 +1,171 @@
+from dataclasses import dataclass
+import typing
+from pathlib import Path
+from typing import List
+
+import libparse
+
+# Ugly hack to get around a recursive import only needed for type checking
+if typing.TYPE_CHECKING:
+    from stubs.interface_spec import InterfaceSpec
+
+
+class InterfaceDef:
+    name: str
+    version: str
+    flags: List[str]
+    spec: 'InterfaceSpec'
+    functions: List['Function']
+
+    def __init__(self, name, version, flags, spec):
+        self.name = name
+        self.version = version
+        self.flags = flags
+        self.spec = spec
+        self.functions = _read_headers(self)
+        print(version, name, flags)
+        super().__init__()
+        pass
+
+    def varname(self):
+        return self.name
+
+    def has_flag(self, flag):
+        return flag in self.flags
+
+    def header_filename(self):
+        return "OpenVR/interfaces/%s.h" % self.interface_v()
+
+    def header_prefix(self):
+        return "SplitOpenVRHeaders"
+
+    def basename(self):
+        return "Base" + self.name
+
+    def base_header(self):
+        return self.basename() + ".h"
+
+    def getter_name(self):
+        return self.basename()
+
+    def proxy_class_name(self):
+        return "CVR%s_%s" % (self.name, self.version)
+
+    def interface(self):
+        return "IVR%s" % self.name
+
+    def interface_v(self):
+        return "%s_%s" % (self.interface(), self.version)
+
+    def namespace(self):
+        """
+        Get the namespace in which this interface class resides, such as vr::IVRSystem_020
+        """
+        return "vr::" + self.interface_v()
+
+    def version_variable(self):
+        return f"{self.namespace()}::{self.interface()}_Version"
+
+    # Throw an error to make sure we don't accidentally write this instance to a file
+    def __str__(self):
+        raise Exception("repl")
+
+
+class CustomInterface(InterfaceDef):
+    def header_filename(self):
+        return "OpenVR/custom_interfaces/%s.h" % (self.interface_v())
+
+
+class DriverInterface(InterfaceDef):
+    def interface_v(self):
+        return "driver_%s_%s" % (self.interface(), self.version)
+
+
+class APIInterface(InterfaceDef):
+    def header_filename(self):
+        return "API/I%s_%s.h" % (self.name, self.version)
+
+    def header_prefix(self):
+        return "OpenOVR"
+
+    def proxy_class_name(self):
+        return "CVROC%s_%s" % (self.name, self.version)
+
+    def basename(self):
+        return "OCBase" + self.name
+
+    def interface(self):
+        return "IVROC%s" % self.name
+
+    def getter_name(self):
+        return "BaseOC" + self.name
+
+    def varname(self):
+        return "OC" + self.name
+
+    def namespace(self):
+        return "ocapi::" + self.interface_v()
+
+    def base_header(self):
+        return "API/" + super().base_header()
+
+
+# Header parsing stuff
+
+_proj_root = Path(__file__).parent.parent.parent
+
+
+@dataclass
+class Function:
+    name: str
+    return_type: str
+    args: List[libparse.arg_t]
+    user_implemented: bool = False
+
+    def args_str(self):
+        """
+        Returns a type-name string of the arguments, as you'd find in a function declaration
+        """
+        return ", ".join(f.type + " " + f.name for f in self.args)
+
+    def args_names(self):
+        """
+        Returns a comma-separated list of the argument names, for passing all the arguments from this function into
+        another function
+        """
+        return ", ".join([a.name for a in self.args])
+
+
+def _read_headers(interface: InterfaceDef) -> List[Function]:
+    filename = _proj_root / interface.header_prefix() / interface.header_filename()
+    context = dict(_get_global_context())
+    libparse.read_context(context, filename, interface.namespace())
+
+    funcs = []
+    with open(filename) as f:
+        for line in libparse.nice_lines(f):
+            func = libparse.parseline(line, context)
+            if not func:
+                continue
+
+            user_funcs = interface.spec.manual_functions.get(interface.proxy_class_name(), None)
+            user_defined = user_funcs is not None and (func.name in user_funcs)
+
+            func = Function(func.name, func.return_type, func.args, user_defined)
+            funcs.append(func)
+
+    return funcs
+
+
+# The global context - that's all the types from vrtypes.h and similar files
+_global_ctx = None
+
+
+def _get_global_context():
+    global _global_ctx
+    if _global_ctx is not None:
+        return _global_ctx
+
+    _global_ctx = dict()
+    libparse.read_context(_global_ctx, _proj_root / "SplitOpenVRHeaders/OpenVR/interfaces/vrtypes.h", "vr")
+    return _global_ctx
