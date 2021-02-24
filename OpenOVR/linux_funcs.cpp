8fd0ea83b3f615f18d04188d9511fbb050876462
Campbell Suter <znix@znix.xyz>
2021-02-24 21:27:17 +1300

    linux: Implement ZeroMemory, strncpy_s and strcpy_s

--- OpenOVR/linux_funcs.cpp
+++ OpenOVR/linux_funcs.cpp
@@ -6,8 +6,23 @@
 
 #ifndef _WIN32
 
-#include "linux_funcs.h"
+void ZeroMemory(void* data, size_t len)
+{
+	memset(data, 0, len);
+}
 
-// TODO implement ZeroMemory, strcpy_s, strncpy_s
+void strncpy_s(char* dest, size_t dest_size, char const* src, size_t max)
+{
+	// Probably not quite right - this is basically the same as strcpy_s - but it should do
+	OOVR_FALSE_ABORT(strlen(src) < max);
+	OOVR_FALSE_ABORT(strlen(src) < dest_size);
+	strncpy(dest, src, dest_size);
+}
+
+void strcpy_s(char* dest, size_t dest_size, char const* src)
+{
+	OOVR_FALSE_ABORT(strlen(src) < dest_size);
+	strcpy(dest, src);
+}
 
 #endif
--- OpenOVR/linux_funcs.h
+++ OpenOVR/linux_funcs.h
@@ -5,5 +5,5 @@
 #pragma once
 
 void ZeroMemory(void* data, size_t len);
-int strncpy_s(char* dest, size_t dest_size, char const* src, size_t max);
-int strcpy_s(char* dest, size_t dest_size, char const* src);
+void strncpy_s(char* dest, size_t dest_size, char const* src, size_t max);
+void strcpy_s(char* dest, size_t dest_size, char const* src);
