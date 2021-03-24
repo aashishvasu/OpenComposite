7af4eceff2a104f1d4ae00c278a846750e6ad86d
Campbell Suter <znix@znix.xyz>
2021-03-24 22:22:48 +1300

    Move the Input*ActionData structs to the new InputData.h header

--- OpenOVR/Reimpl/BaseInput.h
+++ OpenOVR/Reimpl/BaseInput.h
@@ -10,6 +10,8 @@
 #include <string>
 #include <vector>
 
+#include "../Misc/Input/InputData.h"
+
 typedef vr::EVRSkeletalTrackingLevel OOVR_EVRSkeletalTrackingLevel;
 
 enum OOVR_EVRSkeletalReferencePose {
@@ -51,51 +53,6 @@ enum OOVR_EVRInputStringBits {
 	VRInputString_InputSource = 0x04,
 
 	VRInputString_All = 0xFFFFFFFF
-};
-
-struct OOVR_InputAnalogActionData_t {
-	// Whether or not this action is currently available to be bound in the active action set
-	bool bActive;
-
-	// The origin that caused this action's current state
-	vr::VRInputValueHandle_t activeOrigin;
-
-	// The current state of this action; will be delta updates for mouse actions
-	float x, y, z;
-
-	// Deltas since the previous call to UpdateActionState()
-	float deltaX, deltaY, deltaZ;
-
-	// Time relative to now when this event happened. Will be negative to indicate a past time.
-	float fUpdateTime;
-};
-
-struct OOVR_InputDigitalActionData_t {
-	// Whether or not this action is currently available to be bound in the active action set
-	bool bActive;
-
-	// The origin that caused this action's current state
-	vr::VRInputValueHandle_t activeOrigin;
-
-	// The current state of this action; will be true if currently pressed
-	bool bState;
-
-	// This is true if the state has changed since the last frame
-	bool bChanged;
-
-	// Time relative to now when this event happened. Will be negative to indicate a past time.
-	float fUpdateTime;
-};
-
-struct OOVR_InputPoseActionData_t {
-	// Whether or not this action is currently available to be bound in the active action set
-	bool bActive;
-
-	// The origin that caused this action's current state
-	vr::VRInputValueHandle_t activeOrigin;
-
-	// The current state of this action
-	vr::TrackedDevicePose_t pose;
 };
 
 enum OOVR_EVRSkeletalTransformSpace {
