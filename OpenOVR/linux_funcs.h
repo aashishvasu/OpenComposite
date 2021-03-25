04cebb9faad70c6adb81f863047a03076ceb7e8f
Campbell Suter <znix@znix.xyz>
2021-03-25 18:25:07 +1300

    Fix the build failing on Linux

--- OpenOVR/linux_funcs.h
+++ OpenOVR/linux_funcs.h
@@ -7,3 +7,5 @@
 void ZeroMemory(void* data, size_t len);
 void strncpy_s(char* dest, size_t dest_size, char const* src, size_t max);
 void strcpy_s(char* dest, size_t dest_size, char const* src);
+
+#define ARRAYSIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
