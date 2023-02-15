5da3d45fb2e1fc00adc318a5948be09db7c86138
Supreeeme <yungwallace@live.com>
2023-02-15 18:24:04 -0500

    Shutdown session before querying for graphics requirements
    Fixes an issue with Varjo runtimes.

--- DrvOpenXR/XrBackend.cpp
+++ DrvOpenXR/XrBackend.cpp
@@ -168,6 +168,10 @@ void XrBackend::CheckOrInitCompositors(const vr::Texture_t* tex)
 
 		OOVR_LOG("Recreating OpenXR session for application graphics API");
 
+		// Shutdown old session - apparently Varjo doesn't like the session being destroyed
+		// after querying for graphics requirements.
+		DrvOpenXR::ShutdownSession();
+
 		switch (tex->eType) {
 		case vr::TextureType_DirectX: {
 #if defined(SUPPORT_DX) && defined(SUPPORT_DX11)
